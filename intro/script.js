ac0cbdfdd8a025d4804a8789a112b83d8b91d061
melketesfay <melketesfay@gmail.com>
2024-02-21 15:51:27 +0100

    working wave function

--- intro/script.js
+++ intro/script.js
@@ -42,48 +42,89 @@ var t = 0;
 
 
 
-// sine wave with merge
-setInterval(() => {
-    let n = 50+Math.sin(t*0.2)*50;
-    let data = d3.range(0,n,1).map(d=>({
-        x: d*20,
-        y: 200+Math.sin(d*0.2+t)*100
-    }))
+// // sine wave with merge
+// setInterval(() => {
+//     let n = 50+Math.sin(t*0.2)*50;
+//     let data = d3.range(0,n,1).map(d=>({
+//         x: d*20,
+//         y: 200+Math.sin(d*0.2+t)*100
+//     }))
 
-const circleUpdate = svg.selectAll("circle").data(data)
-const circleEnter  = circleUpdate.enter().append("circle").attr("r", 5).attr("fill", "red")
+// const circleUpdate = svg.selectAll("circle").data(data)
+// const circleEnter  = circleUpdate.enter().append("circle").attr("r", 5).attr("fill", "red")
 
-circleUpdate.merge(circleEnter)
-.attr("cx",(d)=>d.x)
-.attr("cy",(d)=>d.y)
+// circleUpdate.merge(circleEnter)
+// .attr("cx",(d)=>d.x)
+// .attr("cy",(d)=>d.y)
 
-t= t+0.01;
+// t= t+0.01;
 
-}, 10);
-
-
+// }, 10);
 
 
-// // sine wave with join and range
+
+
+// // sine wave with join and range path 
 // setInterval(() => {
 
 //     let n = 50 + Math.sin(t)*50
-//     let data = d3.range(0,n,0.1).map(d=>({
-//         x: d*13,
-//         y: 200+Math.sin(d*0.2*t)*100
+//     let data = d3.range(0,100,1).map(d=>({
+//         x: d*15,
+//         y: 400+Math.sin(d*0.4+t)*400,
+//         r: 10+Math.sin(d+t)*5
 //     }))
 
 // const circles = svg.selectAll("circle").data(data)
 // .join("circle")
-// .attr("r", 5)
 // .attr("fill", "red")
 // .attr("cx",(d)=>d.x)
 // .attr("cy",(d)=>d.y)
+// .attr("r", (d)=>d.r)
 
-// t= t+0.1;
+
+// const lineGenerator = d3.line().x((d)=>d.x).y((d)=>d.y);
+// // .curve(d3.curveBasis);
+
+// svg.selectAll("path").data([1])
+
+// .join("path").attr("d",lineGenerator(data)).attr("stroke", "red").attr("fill","none").attr("stroke-width",2);
+
+
+// t= t+0.01;
 
 // }, 1000/60);
 
 
 
 
+
+// sine wave with join and range path 
+setInterval(() => {
+
+    let n = 50 + Math.sin(t)*50
+    let data = d3.range(0,10,1)
+
+    
+    data = data.map(d=>({
+        x: d*130,
+        y: 100- Math.abs(Math.sin(d*0.4+t)*100),
+        r: 10+Math.sin(d+t)*5
+    }))
+
+data[0].y =100;
+data[data.length-1].y=100
+    console.log(data)
+
+
+const lineGenerator = d3.line().x((d)=>d.x).y((d)=>d.y)
+.curve(d3.curveBasis);
+
+svg.selectAll("path").data([1])
+
+.join("path").attr("d",lineGenerator(data)).attr("stroke", "red").attr("fill","red").attr("stroke-width",2);     
+
+
+
+t= t+0.01;
+
+}, 10);
