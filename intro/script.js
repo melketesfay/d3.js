e7f0181741e6ccc48645415ceda8c316f58a0e53
melketesfay <melketesfay@gmail.com>
2024-02-21 17:02:47 +0100

    t

--- intro/script.js
+++ intro/script.js
@@ -1,89 +1,80 @@
-let svg = d3.select('body').append("svg")
-.attr("width", window.innerWidth)
-.attr("height", window.innerHeight)
-
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", window.innerWidth)
+  .attr("height", window.innerHeight);
 
 var t = 0;
 
 // //mesmerize harmonics
 // setInterval(() => {
-//     let data = d3.range(0,100,0.1).map(d=>({
-//         x: d*20,
-//         y: 200+Math.sin(d*0.2*t)*100
-//     }))
+//   let data = d3.range(0, 100, 0.1).map((d) => ({
+//     x: d * 20,
+//     y: 200 + Math.sin(d * 0.2 * t) * 100,
+//   }));
 
-// var circle = svg.selectAll("circle").data(data)
-// circle.enter().append("circle").attr("r", 5).attr("fill", "red")
+//   var circle = svg.selectAll("circle").data(data);
+//   circle.enter().append("circle").attr("r", 5).attr("fill", "red");
 
-// circle.attr("cx",(d)=>d.x)
-// .attr("cy",(d)=>d.y)
+//   circle.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
 
-// t= t+0.1;
-
+//   t = t + 0.1;
 // }, 200);
-
 
 // // full screen mesmerize
 // setInterval(() => {
-//     let data = d3.range(0,100,0.001).map(d=>({
-//         x: d*20,
-//         y: Math.sin(d*Math.PI*t)*(window.innerWidth/2)
-//     }))
+//   let data = d3.range(0, 100, 0.001).map((d) => ({
+//     x: 20 * d,
+//     y: Math.sin(d * Math.PI * t) * (window.innerWidth / 2),
+//   }));
 
-// var circle = svg.selectAll("circle").data(data)
-// circle.enter().append("circle").attr("r", 1).attr("fill", "red")
+//   var circle = svg.selectAll("circle").data(data);
+//   circle.enter().append("circle").attr("r", 1).attr("fill", "red");
 
-// circle.attr("cx",(d)=>d.x)
-// .attr("cy",(d)=>d.y)
+//   circle.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
 
-// t= t+0.1;
+//   t = t + 0.5;
+// }, 1000 / 60);
 
-// }, 200);
+// // sine wave with merge
+// setInterval(() => {
+//   let n = 50 + Math.sin(t) * 50;
+//   let data = d3.range(0, n, 1).map((d) => ({
+//     x: d * 8,
+//     y: 200 + Math.sin(d * 0.2 + t) * 100,
+//   }));
 
+//   const circleUpdate = svg.selectAll("circle").data(data);
+//   const circleEnter = circleUpdate
+//     .enter()
+//     .append("circle")
+//     .attr("r", 5)
+//     .attr("fill", "red");
 
+//   circleUpdate
+//     .merge(circleEnter)
+//     .attr("cx", (d) => d.x)
+//     .attr("cy", (d) => d.y);
 
-// sine wave with merge
-setInterval(() => {
-    let n = 50+Math.sin(t*0.2)*50;
-    let data = d3.range(0,n,1).map(d=>({
-        x: d*20,
-        y: 200+Math.sin(d*0.2+t)*100
-    }))
-
-const circleUpdate = svg.selectAll("circle").data(data)
-const circleEnter  = circleUpdate.enter().append("circle").attr("r", 5).attr("fill", "red")
-
-circleUpdate.merge(circleEnter)
-.attr("cx",(d)=>d.x)
-.attr("cy",(d)=>d.y)
-
-t= t+0.01;
-
-}, 10);
-
-
-
+//   t = t + 0.01;
+// }, 10);
 
 // // sine wave with join and range
 // setInterval(() => {
+//   let n = 50 + Math.sin(t) * 50;
+//   let data = d3.range(0, n, 0.1).map((d) => ({
+//     x: d * 13,
+//     y: 200 + Math.sin(d * Math.PI * t) * 100,
+//   }));
 
-//     let n = 50 + Math.sin(t)*50
-//     let data = d3.range(0,n,0.1).map(d=>({
-//         x: d*13,
-//         y: 200+Math.sin(d*0.2*t)*100
-//     }))
+//   const circles = svg
+//     .selectAll("circle")
+//     .data(data)
+//     .join("circle")
+//     .attr("r", 5)
+//     .attr("fill", "red")
+//     .attr("cx", (d) => d.x)
+//     .attr("cy", (d) => d.y);
 
-// const circles = svg.selectAll("circle").data(data)
-// .join("circle")
-// .attr("r", 5)
-// .attr("fill", "red")
-// .attr("cx",(d)=>d.x)
-// .attr("cy",(d)=>d.y)
-
-// t= t+0.1;
-
-// }, 1000/60);
-
-
-
-
+//   t = t + 0.01;
+// }, 200);
